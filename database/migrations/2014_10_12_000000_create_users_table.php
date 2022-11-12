36ad198f14b63acd05ccf0497964d3d25e337ed4
andreoneres <andreoliveiraneres@gmail.com>
2022-11-12 20:20:28 -0300

    refactor: alter migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,8 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    protected $connection = 'pgsql';
-
     /**
      * Run the migrations.
      *
@@ -16,11 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->id()->autoIncrement();
+            $table->string("name");
+            $table->string("lastname");
+            $table->unsignedTinyInteger("age");
+            $table->char("gender", 1);
+            $table->string("email")->index();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string("password");
             $table->rememberToken();
             $table->timestamps();
         });
